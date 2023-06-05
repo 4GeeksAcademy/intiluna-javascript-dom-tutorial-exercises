cc0271e69477adbd8927a10278768bc246496c29
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-06-05 12:55:50 +0000

    exe9 done

--- exercises/09-Render-on-Click/index.js
+++ exercises/09-Render-on-Click/index.js
@@ -1,5 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-	
+	let myElement = document.createElement("div");
+	myElement.innerHTML = "Hello World";
+	myElement.style.background = "yellow";
+	document.body.appendChild(myElement);
 });
