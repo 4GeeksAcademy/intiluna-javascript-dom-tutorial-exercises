a805f62c6527003448b81f203c836d1974a11a1e
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-06-05 13:15:24 +0000

    exe10 done

--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -1,5 +1,12 @@
 let button = document.querySelector("#superDuperButton");
+//let inTarget = document.querySelector("#myList");
 button.addEventListener("click", function() {
 	//your code here
+	let myElement = document.createElement("li");
+	myElement.innerHTML = "Forth element";
+	//document.body.appendChild(myElement);
+	document.getElementById("myList").appendChild(myElement)
+	//console.log(inTarget.children); 
 
 });
+
