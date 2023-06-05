1fd8d682415bad4165d6b718d0d25a4deaf071f3
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-06-05 13:55:45 +0000

    exe11 done

--- exercises/11-Dynamic-HTML-String/index.js
+++ exercises/11-Dynamic-HTML-String/index.js
@@ -1,2 +1,7 @@
-let myString = "<p>Hello!</p> <strong>My friend</strong>, ";
+const myDate = new Date();
+let thisYear = myDate.getFullYear()
+console.log(thisYear);
+//let myString2 = `<p>we are in the year: ${thisYear} </p> `;
+//let myString = `<p>Hello!</p> <strong>My friend</strong>, we are in the year: ${thisYear}`;
+let myString = "<p>Hello!</p> <strong>My friend</strong>, we are in the year: " + thisYear
 document.write(myString);
