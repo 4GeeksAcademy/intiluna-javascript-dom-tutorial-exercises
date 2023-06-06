db959d0962f0af2dfa88c2d70ae76c752b3fb0d1
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-06-06 19:22:54 +0000

    exe12 p1/2:countries added

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,2 +1,22 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
+
+// append elements
+for(let i=0; i<countries.length;i++){
+    let myElement = document.createElement("option");
+    myElement.innerHTML=countries[i];
+    myElement.value = i;
+    document.getElementById("mySelect").appendChild(myElement);    
+}
+
+// onClick shows alert
+let button = document.querySelector("#mySelect");
+//let inTarget = document.querySelector("#myList");
+button.addEventListener("click", function() {
+	//your code here
+	alert("hi")
+	//console.log(inTarget.children); 
+
+});
+
+
